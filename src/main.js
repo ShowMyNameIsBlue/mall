1b33e4d471d32827e481e0aa95684796124177a5
ShowMyNameIsBlue <1140574499@qq.com>
2019-11-29 20:53:39 +0800

    项目初始化提交

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import '@assets/scss/index.scss'
+import '@assets/js/rem'
+import fastclick from 'fastclick'
+fastclick.attach(document.body)
 Vue.config.productionTip = false
 
 new Vue({
